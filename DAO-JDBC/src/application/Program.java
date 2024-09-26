df140e694b9d0088982dc7c4d22d3e50fdb94c09
Eduardo Decarli <decarliedu@gmail.com>
2024-09-26 16:52:54 -0300

    Department entity class

---
+++ DAO-JDBC/src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Deparment;
+
+public class Program {
+    public static void main(String[] args) {
+        Deparment obj = new Deparment(1, "Books");
+        System.out.println(obj);
+    }
+}
