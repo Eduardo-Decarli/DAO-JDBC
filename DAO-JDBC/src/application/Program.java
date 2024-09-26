582785a53465a35b412a992f474c74cbed7cd8fb
Eduardo Decarli <decarliedu@gmail.com>
2024-09-26 18:07:57 -0300

    Seller entity class

--- DAO-JDBC/src/application/Program.java
+++ DAO-JDBC/src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Deparment;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Deparment obj = new Deparment(1, "Books");
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
